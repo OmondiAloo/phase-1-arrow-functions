3e66f6b1a015451790f59d82e8357cd69b079a44
Emmanuel Omondi Aloo <aloo25emmanuel@gmail.com>
2022-06-10 08:48:24 +0300

    commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let divide = function(x, y) {
+    x=2000
+    y=100
+    return x / y;
+  };
+
+let square = function(x) {
+    
+    return x * x;
+  };
+
+let add = function(a, b) {
+    return a + b;
+  };
